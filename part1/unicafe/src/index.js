94cc531e6d43e4b9e91bbcde02d1e97aacaba817
Daniel Barreiro <33606639+Royallle@users.noreply.github.com>
2019-08-28 23:03:24 +0100

    update unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,49 +1,79 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = (props) => {
+const Statistic = (props) => {
     return (
-        <div>
-            {props.text} {props.counter}
-        </div>
+      <>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </>
     )
 }
 
-const Button = (props) => (
-    <button onClick={props.onClick}>
+const Button = (props) => {
+    return (<button onClick={props.onClick}>
       {props.text}
-    </button>
-  )
+    </button>)
+}
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const increaseGood = () => setGood(good+1)
-  const increaseNeutral = () => setNeutral(neutral+1)
-  const increaseBad = () => setBad(bad+1)
+const Statistics = (props) => {
 
   const sumFeedback = () => {
-      return good+neutral+bad;
+    return props.stats.statsGood+
+           props.stats.statsNeutral+
+           props.stats.statsBad;
   }
+
   const calculateAverage = () => {
     if(sumFeedback() > 0) {
-        return ((good*1)+(bad*-1))/sumFeedback();
+        return (((props.stats.statsGood*1)+(props.stats.statsBad*-1))/sumFeedback()).toFixed(1);
     } else {
         return 0
     }  
   }
 
   const calculatePositivePercentange = () => {
-      if(sumFeedback() > 0) {
-        return (good/sumFeedback())*100;
+      if (sumFeedback() > 0) {
+        return ((props.stats.statsGood/sumFeedback())*100).toFixed(1);
       } else {
           return 0;
       }
   }
 
+  if(sumFeedback() > 0) {
+    return (
+      <div>
+          <table>
+            <tbody>
+              <tr><Statistic text={"good"} value={props.stats.statsGood} /></tr>
+              <tr><Statistic text={"neutral"}  value={props.stats.statsNeutral} /></tr>
+              <tr><Statistic text={"bad"} value={props.stats.statsBad} /></tr>
+              <tr><Statistic text={"all"} value={sumFeedback()} /></tr>
+              <tr><Statistic text={"average"} value={calculateAverage()} /></tr>
+              <tr><Statistic text={"positive"} value={calculatePositivePercentange()+' %'} /></tr>
+            </tbody>
+          </table>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h3>No feedback given</h3>
+      </div>
+    )
+  }
+  
+}  
+
+const App = () => {
+  // save clicks of each button to own state
+  const [counters, setCounters] = useState({
+    good:0,neutral:0,bad:0
+  })
+
+  const increaseGood = () => setCounters({...counters,good: counters.good+1})
+  const increaseNeutral = () => setCounters({...counters,neutral: counters.neutral+1})
+  const increaseBad = () => setCounters({...counters,bad: counters.bad+1})
 
   return (
     <div>
@@ -52,12 +82,7 @@ const App = () => {
         <Button onClick={() => increaseNeutral()} text={"neutral"} />
         <Button onClick={() => increaseBad()} text={"bad"} />
         <h1>statistics</h1>
-        <Display text={"good"} counter={good} />
-        <Display text={"neutral"}  counter={neutral} />
-        <Display text={"bad"} counter={bad} />
-        <Display text={"all"} counter={sumFeedback()} />
-        <Display text={"average"} counter={calculateAverage()} />
-        <Display text={"positive"} counter={calculatePositivePercentange()} />
+        <Statistics stats={{statsGood:counters.good, statsNeutral:counters.neutral, statsBad:counters.bad }}  />
 
     </div>
   )
